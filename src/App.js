42ef769bcc7f5adf4d20ab40f3daebebc20b96ae
been <blueline1318@gmail.com>
2022-09-04 22:01:51 +0900

    [FEAT]: Add basic Route config & install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
+import Quiz from "./pages/Quiz";
+import Result from "./pages/Result";
+
 import "./App.css";
 
 const App = () => {
-  return <Main />;
+  return (
+    <Routes>
+      <Route path="/" element={<Main />}></Route>
+      <Route path="/quiz" element={<Quiz />}></Route>
+      <Route path="/result" element={<Result />}></Route>
+    </Routes>
+  );
 };
 
 export default App;
