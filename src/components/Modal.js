a2a98be8395ce3ef52b8b1ed124c1267a90fd7e7
been <blueline1318@gmail.com>
2022-09-04 21:44:15 +0900

    [STYLE]: Add modal select & button style

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -83,6 +83,31 @@ const ModalBody = styled.div`
   zindex: 1000;
   border: 2px solid #50aa63;
   border-radius: 20px;
+
+  button {
+    display: flex;
+    position: relative;
+    left: 40%;
+    margin: 1rem;
+    padding: 1rem;
+    width: 7rem;
+    height: 3rem;
+    justify-content: center;
+    align-items: center;
+    font-size: 1rem;
+    font-family: "Secular One", sans-serif;
+    color: #fff;
+    background-color: #50aa63;
+    border-radius: 10px;
+    border: none;
+    cursor: pointer;
+  }
+
+  button:hover {
+    color: #50aa63;
+    background-color: #fff;
+    border: 1px solid #50aa63;
+  }
 `;
 
 const ModalContent = styled.div`
--- src/components/QuizSelectBox.js
+++ src/components/QuizSelectBox.js
@@ -1,8 +1,8 @@
-import React from "react";
+import styled from "styled-components";
 
 const QuizSelectBox = ({ label, options, hadleSelectBox }) => {
   return (
-    <>
+    <Wrapper>
       <label htmlFor={label}>{label}</label>
       <select
         id={label}
@@ -16,8 +16,45 @@ const QuizSelectBox = ({ label, options, hadleSelectBox }) => {
           </option>
         ))}
       </select>
-    </>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  position: relative;
+  width: 15em;
+  height: 2em;
+  border-radius: 0.25em;
+  margin: 30px;
+  margin-left: 50px;
+  padding-bottom: 30px;
+  align-items: center;
+  color: #50aa63;
+  font-family: "Secular One", sans-serif;
+
+  label {
+    position: absolute;
+    top: 30%;
+  }
+
+  select {
+    position: absolute;
+    left: 50%;
+    top: 20%;
+    width: 300%;
+    height: 70%;
+    margin: 0;
+    padding: 0.2rem 0.5rem;
+    color: #50aa63;
+    cursor: pointer;
+    border: 1px solid #50aa63;
+    border-radius: 5px;
+  }
+
+  select:hover {
+    outline: 3px solid #50aa63;
+  }
+`;
 
 export default QuizSelectBox;
