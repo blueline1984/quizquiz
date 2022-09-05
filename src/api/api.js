0c2643ab73a328102fed2d3ae08c98aa4fb24985
been <blueline1318@gmail.com>
2022-09-05 10:42:54 +0900

    [CHORE]: Change API URL

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ export const fetchQuizData = async (number, difficulty, category) => {
   const baseURL = process.env.REACT_APP_API_URL;
   const data = await (
     await fetch(
-      `${baseURL}amount=${number}&difficulty=${difficulty}&category=${category}`
+      `${baseURL}amount=${number}&difficulty=${difficulty}&category=${category}&type=multiple`
     )
   ).json();
 
