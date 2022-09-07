ed257aac63a057d2a0bd3d606e43e04c3b08d744
been <blueline1318@gmail.com>
2022-09-07 20:51:57 +0900

    [FEAT]: Add try catch to api call

--- src/api/api.js
+++ src/api/api.js
@@ -1,18 +1,22 @@
 import { shuffleArray } from "../utils/utils";
 
 export const fetchQuizData = async (number, difficulty, category) => {
-  const baseURL = process.env.REACT_APP_API_URL;
-  const data = await (
-    await fetch(
-      `${baseURL}amount=${number}&difficulty=${difficulty}&category=${category}&type=multiple`
-    )
-  ).json();
+  try {
+    const baseURL = process.env.REACT_APP_API_URL;
+    const data = await (
+      await fetch(
+        `${baseURL}amount=${number}&difficulty=${difficulty}&category=${category}&type=multiple`
+      )
+    ).json();
 
-  return data.results.map((quizData) => ({
-    ...quizData,
-    answers: shuffleArray([
-      ...quizData.incorrect_answers,
-      quizData.correct_answer,
-    ]),
-  }));
+    return data.results.map((quizData) => ({
+      ...quizData,
+      answers: shuffleArray([
+        ...quizData.incorrect_answers,
+        quizData.correct_answer,
+      ]),
+    }));
+  } catch (error) {
+    throw new Error("Data Fetch API error");
+  }
 };
